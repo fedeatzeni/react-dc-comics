1ff034130172592127623dc7e667d70ee67dee55
Fede <fede.atzeni372@gmail.com>
2025-02-12 18:04:23 +0100

    feat main and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,69 @@
 export default function Footer() {
     return (
         <footer>
-            
+            <div className="footer-nav">
+                <div>
+                    <div className="nav">
+                        <div className="col">
+                            <h2>DC COMICS</h2>
+                            <a href="">Characters</a>
+                            <a href="">Comics</a>
+                            <a href="">Movies</a>
+                            <a href="">TV</a>
+                            <a href="">Games</a>
+                            <a href="">Videos</a>
+                            <a href="">News</a>
+
+                            <br />
+                            <h2>SHOP</h2>
+                            <a href="">Shop</a>
+                            <a href="">Shop Collectionables</a>
+                        </div>
+
+                        <div className="col">
+                            <h2>DC</h2>
+                            <a href="">Terms Of Use</a>
+                            <a href="">Privacy policy</a>
+                            <a href="">Ad Choises</a>
+                            <a href="">Advertising</a>
+                            <a href="">Jobs</a>
+                            <a href="">Subscription</a>
+                            <a href="">Talent Workshows</a>
+                            <a href="">CPSC Certificates</a>
+                            <a href="">Rating</a>
+                            <a href="">Shop Help</a>
+                            <a href="">Contact Us</a>
+                        </div>
+
+                        <div className="col">
+                            <h2>SITES</h2>
+                            <a href="">DC</a>
+                            <a href="">MAD Magazine</a>
+                            <a href="">DC Kids</a>
+                            <a href="">DC Universe</a>
+                            <a href="">DC Power Visa</a>
+
+                        </div>
+                    </div>
+
+                    <img src="./img/dc-logo-bg.png" alt="logo" />
+                </div>
+            </div>
+
+            <div className="footer-social">
+                <div>
+                    <div className="singup">Sing Up</div>
+                    <div className="social">
+                        <div>Follow Us</div>
+                        <img src="./img/footer-facebook.png" alt="" />
+                        <img src="./img/footer-twitter.png" alt="" />
+                        <img src="./img/footer-youtube.png" alt="" />
+                        <img src="./img/footer-pinterest.png" alt="" />
+                        <img src="./img/footer-periscope.png" alt="" />
+                    </div>
+                </div>
+
+            </div>
         </footer>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,9 @@
 export default function Main() {
     return (
         <main>
-            
+            <div>
+                <h1>Content here</h1>
+            </div>
         </main>
     )
 }
