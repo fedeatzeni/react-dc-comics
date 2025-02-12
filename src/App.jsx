eda250240dc58aa9109559c1c7b6613891bf715b
Fede <fede.atzeni372@gmail.com>
2025-02-12 19:11:44 +0100

    fix layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Header from "./components/Header"
-import Main from "./components/Main"
-import Footer from "./components/Footer"
+import Header from "./components/Header.jsx"
+import Main from "./components/Main.jsx"
+import Footer from "./components/Footer.jsx"
 
 function App() {
   return (
---
+++ src/components/MainNav.jsx
@@ -0,0 +1,8 @@
+export default function MainNav() {
+    <div>
+        <div>
+            <img src="./img/buy-comics-digital-comics.png" alt="" />
+            <h2>DIGITAL COMICS</h2>
+        </div>
+    </div>
+}
