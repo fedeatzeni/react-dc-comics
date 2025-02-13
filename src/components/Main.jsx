3ccd392f404b9de42a5857e0215dd6b446ecdeae
Fede <fede.atzeni372@gmail.com>
2025-02-13 14:45:48 +0100

    feat mainnav

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,13 @@
+import MainNav from "./MainNav.jsx"
+
+
 export default function Main() {
     return (
         <main>
             <div>
                 <h1>Content here</h1>
             </div>
+            <MainNav />
         </main>
     )
 }
--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,8 +1,34 @@
 export default function MainNav() {
-    <div>
-        <div>
-            <img src="./img/buy-comics-digital-comics.png" alt="" />
-            <h2>DIGITAL COMICS</h2>
+    return (
+        <div className="container">
+            <div>
+                <div className="card">
+                    <img src="./img/buy-comics-digital-comics.png" alt="" />
+                    <a href="">DIGITAL COMICS</a>
+                    <a href=""></a>
+                </div>
+
+                <div className="card">
+                    <img src="./img/buy-comics-merchandise.png" alt="" />
+                    <a href="">DIGITAL COMICS</a>
+                </div>
+
+                <div className="card">
+                    <img src="./img/buy-comics-subscriptions.png" alt="" />
+                    <a href="">DIGITAL COMICS</a>
+                </div>
+
+                <div className="card">
+                    <img src="./img/buy-comics-shop-locator.png" alt="" />
+                    <a href="">DIGITAL COMICS</a>
+                </div>
+
+                <div className="card">
+                    <img src="./img/buy-dc-power-visa.svg" alt="" />
+                    <a href="">DIGITAL COMICS</a>
+                </div>
+
+            </div>
         </div>
-    </div>
+    )
 }
