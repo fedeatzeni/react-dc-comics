7d1b70a9e0491f86cc112c4c30ee65f5984ae68a
Fede <fede.atzeni372@gmail.com>
2025-02-14 13:45:28 +0100

    feat prop drilling

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,23 @@ import Main from "./components/Main.jsx"
 import Footer from "./components/Footer.jsx"
 
 function App() {
+
+  const links = [
+    { id: 1, name: "CHARACTERS", url: "#", current: false },
+    { id: 2, name: "COMICS", url: "#", current: true },
+    { id: 3, name: "MOVIES", url: "#", current: false },
+    { id: 4, name: "TV", url: "#", current: false },
+    { id: 5, name: "GAMES", url: "#", current: false },
+    { id: 6, name: "COLLECTIBLES", url: "#", current: false },
+    { id: 7, name: "VIDEOS", url: "#", current: false },
+    { id: 8, name: "FANS", url: "#", current: false },
+    { id: 9, name: "NEWS", url: "#", current: false },
+    { id: 10, name: "SHOP", url: "#", current: false },
+  ]
+
   return (
     <>
-      <Header></Header>
+      <Header linksProps={links}></Header>
       <Main></Main>
       <Footer></Footer>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,14 @@
 import Navbar from "./Navbar.jsx"
 
-export default function Header() {
+export default function Header(props) {
 
-    const links = [
-        { id: 1, name: "CHARACTERS", url: "#", current: false },
-        { id: 2, name: "COMICS", url: "#", current: true },
-        { id: 3, name: "MOVIES", url: "#", current: false },
-        { id: 4, name: "TV", url: "#", current: false },
-        { id: 5, name: "GAMES", url: "#", current: false },
-        { id: 6, name: "COLLECTIBLES", url: "#", current: false },
-        { id: 7, name: "VIDEOS", url: "#", current: false },
-        { id: 8, name: "FANS", url: "#", current: false },
-        { id: 9, name: "NEWS", url: "#", current: false },
-        { id: 10, name: "SHOP", url: "#", current: false },
-    ]
+
 
     return (
         <header>
             <img src="./img/dc-logo.png" alt="" />
 
-            <Navbar linksProps={links} />
+            <Navbar linksProps={props.linksProps} />
         </header>
     )
 }
